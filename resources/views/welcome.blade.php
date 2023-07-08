a87a4aee31c665bf5ff8f28444789022f1fc74f1
Harry Saputra <flexnewbie@gmail.com>
2023-07-08 22:02:45 +0700

    Revert "set trustproxies, mix content issue"
    This reverts commit 5106e56f99c8f931e094b628371b8b9e537b34fe.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,8 @@
   @vite('resources/css/app.css')
 </head>
 <body>
-    <main class="max-w-4xl mx-auto mt-8">
-        <div class=" grid grid-cols-4 gap-4">
-            @foreach($boxes as $box)
-                <div class="border p-4 border-gray-800" style="{{ $box['style'] }}">
-                    {{ $box['boxNumber'] }}. {{ $box['text'] }}
-                </div>
-            @endforeach
-        </div>
-    </main>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </body>
 </html>
